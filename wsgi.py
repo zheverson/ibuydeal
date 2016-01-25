48589068fcdf07a3cd8af8e69896d0169dee41cf
zheverson <172329313@qq.com>
2016-01-25 18:22:43 +0800

    sa wq:

---
+++ wsgi.py
@@ -0,0 +1,23 @@
+import model
+import simplejson as json
+
+
+def app(environ, start_response):
+    body = response(environ)
+    start_response("200 ok", [("Content-type", "application/json")])
+    return [body]
+
+
+def response(environ):
+    method = environ['REQUEST_METHOD']
+    uri = environ['RAW_URI']
+    uri_parse = uri.split('/')
+    first_uri = uri_parse[1]
+    if first_uri == "user":
+        dbtuple = model.User.get_feeds()
+        dbjson = json.dumps([{"id": i[0], "ratio": i[1]} for i in dbtuple])
+        return dbjson.encode('utf-8')
+    else:
+        return "what do you want?".encode('utf-8')
+
+
